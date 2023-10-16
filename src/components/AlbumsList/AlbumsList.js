02eaebee4276dacd86df9b2bee784922f2ce1f3b
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-16 10:46:57 +0530

    changes to css

--- src/components/AlbumsList/AlbumsList.js
+++ src/components/AlbumsList/AlbumsList.js
@@ -27,30 +27,33 @@ function Album({ data, options, index, handleOptions }) {
   const [isOptionsClicked, setIsOptionsClicked] = useState(false);
   return (
     <div className="album">
-      <div className="stack stack-1"></div>
-      <div className="stack stack-2"></div>
-      <div
-        className="album-img-box"
-        onClick={() => console.log("CLicked")}
-        onMouseOver={() => handleOptions(index)}
-        onMouseOut={() => handleOptions(null)}
-      >
-        {options === index && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            className="options"
-          >
-            <path
-              fill="currentColor"
-              d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0zm0-6a2 2 0 1 0 4 0a2 2 0 0 0-4 0zm0 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0z"
-            ></path>
-          </svg>
-        )}
-
-        <img className="album-img" src={data.images.at(0)} alt={`${data.id}`} />
+      <div className="helper">
+        <div className="stack stack-1"></div>
+        <div className="stack stack-2"></div>
+        <div
+          className="album-img-box"
+          onClick={() => console.log("CLicked")}
+          onMouseOver={() => handleOptions(index)}
+          onMouseOut={() => handleOptions(null)}
+        >
+          <img
+            className="album-img"
+            src={data.count < 1 ? "/default.jpg" : data.images.at(0)}
+            alt={`${data.id}`}
+          />
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          className="options"
+        >
+          <path
+            fill="currentColor"
+            d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0zm0-6a2 2 0 1 0 4 0a2 2 0 0 0-4 0zm0 12a2 2 0 1 0 4 0a2 2 0 0 0-4 0z"
+          ></path>
+        </svg>
       </div>
 
       <div className="album-label">
