1b68d4694abca483c86bf9fb48257e56fb8cd165
Bhavya-Goyal423 <Goel2000bhavya@gmail.com>
2023-10-17 08:45:45 +0530

    changes to css

--- src/components/ImagesList/ImageList.js
+++ src/components/ImagesList/ImageList.js
@@ -1,11 +1,16 @@
 import "./ImageList.css";
+import ImageGallery from "react-image-gallery";
 
 export default function ImageList({ allImages, selectedAlbum }) {
   const arr1 = [];
   const arr2 = [];
   const arr3 = [];
 
+  const images = [];
+
   allImages.forEach((el, idx) => {
+    const newEntry = { original: el, thumbnail: "" };
+    images.push(newEntry);
     if (idx % 3 === 0) {
       arr1.push(el);
     } else if (idx % 3 === 1) {
@@ -13,21 +18,47 @@ export default function ImageList({ allImages, selectedAlbum }) {
     } else arr3.push(el);
   });
 
+  const handleimg = (e) => {
+    const src = e.target.src;
+    const index = allImages.indexOf(src);
+  };
+
+  console.log(images);
+
   return (
     <div className="main-grid">
+      <ImageGallery items={images} />;
       <div className="grid-1">
-        {arr1.map((el) => (
-          <img className="img" src={el} alt="" />
+        {arr1.map((el, idx) => (
+          <img
+            key={el}
+            className="img"
+            src={el}
+            alt=""
+            onClick={(e) => handleimg(e)}
+          />
         ))}
       </div>
       <div className="grid-2">
-        {arr2.map((el) => (
-          <img className="img" src={el} alt="" />
+        {arr2.map((el, idx) => (
+          <img
+            key={el}
+            className="img"
+            src={el}
+            alt=""
+            onClick={(e) => handleimg(e)}
+          />
         ))}
       </div>
       <div className="grid-3">
-        {arr3.map((el) => (
-          <img className="img" src={el} alt="" />
+        {arr3.map((el, idx) => (
+          <img
+            key={el}
+            className="img"
+            src={el}
+            alt=""
+            onClick={(e) => handleimg(e)}
+          />
         ))}
       </div>
     </div>
